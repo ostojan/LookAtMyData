0859cb0520716e96791faeb9ea029ac302b41971
Aleksander Stojanowski <root.zelatyna.0f@icloud.com>
2024-08-08 21:49:01 +0200

    Init commit

---
+++ LookAtMyData/LookAtMyDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  LookAtMyDataApp.swift
+//  LookAtMyData
+//
+//  Copyright (c) 2024 Aleksander Stojanowski. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct LookAtMyDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
